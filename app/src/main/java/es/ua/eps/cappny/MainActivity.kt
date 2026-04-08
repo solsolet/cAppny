a6ac7b0a11b4cfe86c2a5cf4382ac6460002124d
solsolet <gsl21@alu.ua.es>
2026-04-08 23:56:02 +0200

    Muntada inicial

---
+++ app/src/main/java/es/ua/eps/cappny/MainActivity.kt
@@ -0,0 +1,18 @@
+package es.ua.eps.cappny
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import es.ua.eps.cappny.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
